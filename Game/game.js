7d76a29ce9258dc0c2fa30dce63ff4542cfe2927
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-28 02:46:37 +0530

    fetching questions form external file -- issue have to define the size of the questions array

--- Game/game.js
+++ Game/game.js
@@ -9,49 +9,23 @@ let questionCounter = 0;
 let availableQuestions = [];
 // questions array
 
-let questions = [
-  {
-    question: "Inside which HTML element dp we put the JavaScript",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1,
-  },
-  {
-    question: "Which of the following keyword is used to create an object in Java",
-    choice1: "create",
-    choice2: "new",
-    choice3: "implements",
-    choice4: "extends",
-    answer: 2,
-  },
-  {
-    question: "Find the next one 2,A,9,B,6,C,13,D",
-    choice1: "9",
-    choice2: "10",
-    choice3: "E",
-    choice4: "15",
-    answer: 2,
-  },
-  {
-    question: "Which character is used in Python to make a single line comment",
-    choice1: "/",
-    choice2: "//",
-    choice3: "#",
-    choice4: "!",
-    answer: 3,
-  },
-  {
-    question: "JavaScript is",
-    choice1: "Server-side scripting language",
-    choice2: "Client-side scripting language",
-    choice3: "Both of the above",
-    choice4: "None of these",
-    answer: 3,
-  },
-];
+//using fetch API to get the question from local file i.e questions.json
+let questions = [{},{},{},{},{}];
 
+//promise function?? response
+//fetch rturns a promise
+fetch("../questions.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    // console.log(loadedQuestions);
+    questions = loadedQuestions;
+    console.log(loadedQuestions.length);
+    startGame();
+  }).catch(err =>{
+    console.error(err);
+  });
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = questions.length;
 
@@ -60,12 +34,12 @@ startGame = () => {
   score = 0;
   availableQuestions = [...questions];
   //   using spread operator making an entire copy of the questions array
-  console.log(availableQuestions);
+  console.log(availableQuestions.length);
   getNewQuestion();
 };
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    localStorage.setItem('mostRecentScore', score);
+    localStorage.setItem("mostRecentScore", score);
     //go to the end page if the questions array is empty
     return window.location.assign("../end page/end.html");
   }
@@ -113,4 +87,3 @@ incrementScore = (num) => {
   score += num;
   scoreText.innerText = score;
 };
-startGame();
